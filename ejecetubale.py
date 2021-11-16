1ddfdea66e5887c2ac521418ed8753944e5a69ea
nick-0318 <91357439+nick-0318@users.noreply.github.com>
2021-11-15 23:43:45 -0500

    Update ejecetubale.py

--- ejecetubale.py
+++ ejecetubale.py
@@ -8,12 +8,15 @@ Created on Mon Nov 15 18:37:13 2021
 
 import regresion1 as r1
 
-data = r1.pregunta()
-header = r1.header(data)
-datay = r1.extraey(data, header)
-variable = r1.extraerdatos(data, header)
-exp = r1.extraedatos2(data, header, variable)
-Matrix = r1.matrizmarkov(exp, variable)
-y = r1.regresionlineal(Matrix, datay)
+print("Este programa le permite realizar una regresión lineal por el método de minimos cuadrados ordinarios, para continuar escriba *continuar* y llene la información necesaria) 
+a=input("Ingrese")
+if a==continuar:
+  data = r1.pregunta()
+  header = r1.header(data)
+  datay = r1.extraey(data, header)
+  variable = r1.extraerdatos(data, header)
+  exp = r1.extraedatos2(data, header, variable)
+  Matrix = r1.matrizmarkov(exp, variable)
+  y = r1.regresionlineal(Matrix, datay)
 
 
